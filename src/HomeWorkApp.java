b3b177ca95464d38bf18aa59f8a018bb63ef4771
Tanisat <atanisat@gmail.com>
2022-02-17 20:10:09 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,27 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSum(4, 15);
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSum(int a, int b) {
+        int sum;
+        sum = a + b;
+        if (sum>=0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor(int value) {
+        if (value<0)
+    }
+
+}
